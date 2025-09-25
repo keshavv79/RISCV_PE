bbd931ca285ce1efaac5fe9d5d72787bd8b12460
Anshul Patil <116883005+ColonelZod16@users.noreply.github.com>
2025-09-25 22:23:03 +0530

    Update tb_booth_radix4_multiplier_8bit.v

--- tb_booth_radix4_multiplier_8bit.v
+++ tb_booth_radix4_multiplier_8bit.v
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/25/2025 07:00:55 PM
+// Design Name: 
+// Module Name: tb_booth_radix4_multiplier_8bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module tb_booth_radix4_multiplier_8bit;
 
   reg signed [31:0] multiplicand;
@@ -48,40 +70,40 @@ module tb_booth_radix4_multiplier_8bit;
         #20;
 
         // Test case 1: 3 * 5 = 15
-        multiplicand = 64'sd1000000;
-        multiplier = 64'sd10000074;
+        multiplicand = 32'sd10000;
+        multiplier = -32'sd829742;
         start = 1; #10; start = 0;
         wait(done);
         #100;
         $display("3 * 5 = %d, Product = %d", 3*5, product);
 
         // Test case 2: 7 * -7 = -49
-        multiplicand = 64'sd7;
-        multiplier = -64'sd7;
+        multiplicand = 32'sd7;
+        multiplier = -32'sd7;
         start = 1; #10; start = 0;
         wait(done);
         #10;
         $display("7 * -7 = %d, Product = %d", 7*(-7), product);
 
         // Test case 3: -5 * -9 = 45
-      multiplicand = -64'sd5;
-      multiplier = -64'sd9;
+      multiplicand = -32'sd5;
+      multiplier = -32'sd9;
         start = 1; #10; start = 0;
         wait(done);
         #10;
         $display("-5 * -9 = %d, Product = %d", (-5)*(-9), product);
 
         // Test case 4: 0 * 11 = 0
-        multiplicand = 64'sd0;
-        multiplier = 64'sd11;
+        multiplicand = 32'sd0;
+        multiplier = 32'sd11;
         start = 1; #10; start = 0;
         wait(done);
         #10;
         $display("0 * 11 = %d, Product = %d", 0*11, product);
 
         // Test case 5: -8 * 4 = -32
-        multiplicand = -64'sd8;
-        multiplier = 64'sd4;
+        multiplicand = -32'sd8;
+        multiplier = 32'sd4;
         start = 1; #10; start = 0;
         wait(done);
         #10;
